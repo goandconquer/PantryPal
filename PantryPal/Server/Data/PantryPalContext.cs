fd9bd10c3eb955a196ae076b00925679aca8683b
David Ray Goforth <goforthd@dlstx.org>
2023-09-11 16:34:54 -0500

    add PantryPalContext

---
+++ PantryPal/Server/Data/PantryPalContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PantryPal.Server.Models;
+
+namespace PantryPal.Server.Data;
+
+public class PantryPalContext : DbContext
+{
+    public PantryPalContext(DbContextOptions<PantryPalContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<FoodItem> FoodItems { get; set; }
+}
