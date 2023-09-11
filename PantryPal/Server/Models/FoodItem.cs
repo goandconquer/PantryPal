0ab885c95e1bbff62d5bd7e8f6d7f616fcd4e0a2
David Ray Goforth <goforthd@dlstx.org>
2023-09-11 16:32:47 -0500

    add FoodItem class

---
+++ PantryPal/Server/Models/FoodItem.cs
@@ -0,0 +1,10 @@
+﻿namespace PantryPal.Server.Models;
+
+public class FoodItem
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public int Quantity { get; set; }
+}
